d747958e659a90851e08425daa680b2223b47eb1
Sunira Moses <sunira.moses@gmail.com>
2020-05-13 00:15:31 -0400

    Updated to use PnP Cmdlets for Modern Site Generation

--- project-generator.ps1
+++ project-generator.ps1
@@ -1,46 +1,38 @@
 function new-M365Site {
     
     #Retrieve the top level site from the user.
-    $tenantURL = Read-Host "Please Provide the Tenant/Admin Site URL"
+    $tenantURL = Read-Host "Please Provide the Tenant/Admin Site URL:"
 
-    #Retrieve the site url the user wants to provision.
-    $url = Read-Host "Please enter the SPO URL for the site being created"
-
-    #Retrieve the site title from User.
+    #Retrieve the site title from User
     $title = Read-Host "Please enter the title of the new site"
 
-    #Retrieve the site owner e-mail from User.
-    $owner = Read-Host "Please enter the site owner e-mail for this new site"
+    #Retrieve the site title from User
+    $desc = Read-Host "Please enter the site description of the new site"
 
-    # Connect to M365
+    #Retrieve the site alias ( site.sharepoint.com/sites/*alias* ) from User
+    $alias = Read-Host "Please enter the site alias of the new site. example: ...com/sites/your-alias"
+
+    #Connect to M365
     try {
-        Connect-SPOService -Url $tenantURL
+        Connect-PnPOnline $tenantURL
+        #Connect-SPOService -Url $tenantURL -Credential $credentials
         write-host "Connected succesfully to M365!" -foregroundcolor green
     }
     catch {
         write-host "Failed to connect to M365." -foregroundcolor red
         Break new-M365Site
     }
-    
-    #Does the site already exist in SPO?
-    $siteExists = get-SPOSite | Where-Object { $_.url -eq $url }
-    
-    #Or does the site exist in the Trash? 
-    $isSiteInRBin = get-SPODeletedSite | Where-Object { $_.url -eq $url }
-    
-    #If it doesn't already exist in those two places, create the site. 
 
-    write-host "Attempting to create site: $($title)" -foregroundcolor green
-    if (($null -eq $siteExists) -and ( $null -eq $isSiteInRBin)) {
-        New-SPOSite -Url $url -Owner $owner -StorageQuota 100 -Title $title -Template "PROJECTSITE#0"
-        write-host "$($teamSiteUrl) created!" -foregroundcolor green
+    #Try to create new TeamSite(with O365 Group)
+    try {        
+        write-host "Attempting to create site: $($title)" -foregroundcolor green
+        $teamSiteUrl = New-PnPSite -Type TeamSite -Title $title -Description $desc -Alias $alias
+        write-host "Successfully Created: $($teamSiteUrl)! " -foregroundcolor green
     }
-    elseif ($siteExists -eq $true) {
-        write-host "info: $($url) already exists" -foregroundcolor red
-    }
-    else {
-        write-host "info: $($url) still exists in the Recycling Bin" -foregroundcolor red
-    }
+    catch {
+        write-host "Failed to create new site: $($Title)." -foregroundcolor red
+        Break new-M365Site
+    }   
 }
 
 new-M365Site
